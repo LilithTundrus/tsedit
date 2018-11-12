27cdd89da8836c9d70bd4f2e50bb377d958dbdfe
Lilith Tundrus <echofox123@gmail.com>
2018-11-11 22:44:32 -0500

    Adding entry point for project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+// The above line tells OS that this is NOT a shell script, and needs a specific interpreter
+
+// Using ES6 strict mode (not 100% needed, but ensure that the compiled JS is in strict mode)
+'use strict';
+
+
+// Node/NPM dependencies
+
+console.log('Test');
