952608e21f0efdebd1d4d08ef6f4a7796f5f03ed
Lilith Tundrus <echofox123@gmail.com>
2018-11-13 03:58:13 -0500

    Adding some more basic class data and delcarations

--- src/editor/Editor.ts
+++ src/editor/Editor.ts
@@ -2,8 +2,7 @@
 'use strict';
 
 // Node/NPM dependencies
-
-
+import * as blessed from 'blessed';
 
 // This is the main editor class that puts all of the pieces together 
 // to create a functioning application
@@ -11,8 +10,31 @@
 
 export default class Editor {
     private filePath: string;
+
+    // The editor's 'state' is whether or not it's editing a new or existing file
     private editorState: string;
 
+    // Create the blessed program object to associate with the blessed screen for the class
+    private program = blessed.program();
+
+    // Declared as any since blessed's typings aren't correct
+    private cursorOptions: any = {
+        artificial: true,
+        shape: 'line',
+        blink: true,
+        color: null
+    }
+
+    private screen = blessed.screen({
+        smartCSR: true,
+        // Autopad screen elements unless no padding it explicitly required
+        autoPadding: true,
+        // Associate the generated program to the screen
+        program: this.program,
+        // Used, but often doesn't work in windows
+        cursor: this.cursorOptions
+    });
+    
     constructor(filePath?: string) {
         this.filePath = filePath;
 
@@ -21,6 +43,10 @@ export default class Editor {
         }
     }
 
+    /** Start the editor in a state where an empty file is being worked on
+     * @private
+     * @memberof Editor
+     */
     private startEditorBlank() {
 
     }
