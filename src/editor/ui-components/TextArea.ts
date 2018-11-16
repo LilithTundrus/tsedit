cbb11399579aacd076c5ff16c2af5dbd408ef3cb
Lilith Tundrus <echofox123@gmail.com>
2018-11-16 18:38:20 -0500

    Creating the TextArea class file to later integrate with the main Editor class

---
+++ src/editor/ui-components/TextArea.ts
@@ -0,0 +1,16 @@
+// Using ES6 strict mode (not 100% needed, but ensure that the compiled JS is in strict mode)
+'use strict';
+
+// Node/NPM dependencies
+import * as blessed from 'blessed';
+
+// This file contains one of the blessed components for constructing the UI in an effort to
+// keep this project modular
+
+// Create the textArea textbox, where the actual text being edited will be displayed
+
+class textArea {
+    constructor() {
+        
+    }
+}
