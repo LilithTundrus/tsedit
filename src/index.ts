1f0d75bc3297661c50d86e459b5ac764ab024f1e
Lilith Tundrus <echofox123@gmail.com>
2018-11-16 17:30:25 -0500

    Correctly launching thed editor now.

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,17 @@
 // Using ES6 strict mode (not 100% needed, but ensure that the compiled JS is in strict mode)
 'use strict';
 
+// Local dependencies
+import Editor from "./editor/Editor";
+
 
 // Main entry point for the editor, check for an argument, 
 // if not then launch a blank editor
 
 if (process.argv[2]) {
     // Perform the operations to attempt to read/open a file
-    console.log(process.argv[2]);
+    const editor = new Editor(process.argv[2]);
 } else {
     // Launch the edtior in an empty mode and in the unsaved state
+    const editor = new Editor();
 }
