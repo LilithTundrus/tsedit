e024bd1a71fa2704631037fff7bcf19ccab0f72e
Lilith Tundrus <echofox123@gmail.com>
2018-11-18 01:37:31 -0500

    Creating a class to handle all keypresses within the textArea UI component

---
+++ src/editor/KeyHandler.ts
@@ -0,0 +1,17 @@
+// Using ES6 strict mode (not 100% needed, but ensure that the compiled JS is in strict mode)
+'use strict';
+
+// Node/NPM dependencies
+import * as blessed from 'blessed';
+import Editor from './Editor';
+
+
+// This file contains the class for handling key events for the Editor Class's
+// textArea UI component
+
+class KeyHandler {
+
+    constructor(editorInstance) {
+
+    }
+}
