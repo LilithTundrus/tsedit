41a7c09b16e6897774e551e0760f22b1bcab7107
Lilith Tundrus <echofox123@gmail.com>
2018-11-27 01:28:21 -0500

    Working on an enterHandler refactor to work like the main keyHandler, still in progress...

--- src/editor/KeyHandler.ts
+++ src/editor/KeyHandler.ts
@@ -303,30 +303,8 @@ export default class KeyHandler {
             let currentLineText = this.editorInstance.textArea.textArea.getLine(currentLineOffset);
 
             // If cursor is at the beginning of the line
-            if (cursor.x == 2 && currentLineText.length >= 1) {
-                // Insert a blank line ABOVE the current line so the content flows down by one,
-                // copying how a lot of editors work when enter is hit at the start of a line
-                this.editorInstance.textArea.textArea.insertLine(currentLineOffset, '');
-                // Render the line change
-                this.editorInstance.screen.render();
-
-                // Set the cursor back to the beginning of the current line if 
-                // it is not at the bottom of the textArea
-                if (cursor.y < this.editorInstance.screen.height - 1) {
-                    this.editorInstance.program.cursorPos(cursor.y, 1);
-                }
-                else {
-                    // Scroll the textArea by one
-                    this.editorInstance.textArea.textArea.scroll(1);
-                    // Put the cursor at the start of the current line
-                    this.editorInstance.program.cursorPos(cursor.y - 1, 1);
-                    // Increase the verticalScrollOffset by one to match the blessed scroll index
-                    this.editorInstance.textArea.verticalScrollOffset++;
-                }
-                // Render the cursor change
-                this.editorInstance.screen.render();
-                // update the line to in the 'real' text
-                this.editorInstance.textArea.shadowContent.splice(currentLineOffset, 0, '');
+            if (cursor.x == 2) {
+                this.enterHandlerStartOfLine(cursor);
             }
             // Cursor is at the 'end' of the line (still checks for the viewOffset)
             else if (cursor.x >= this.editorInstance.screen.width - 1) {
@@ -364,7 +342,40 @@ export default class KeyHandler {
                 // along with handling when the line 
             }
         });
+    }
 
+    enterHandlerStartOfLine(cursor) {
+        // Variable to get the current offset number for the line the cursor is on,
+        // including the scrolling position of the textArea
+        let currentLineOffset = this.editorInstance.textArea.calculateScrollingOffset(cursor);
+
+        // Insert a blank line ABOVE the current line so the content flows down by one,
+        // copying how a lot of editors work when enter is hit at the start of a line
+        this.editorInstance.textArea.textArea.insertLine(currentLineOffset, '');
+        // Render the line change
+        this.editorInstance.screen.render();
+
+        if (this.editorInstance.textArea.viewOffSet > 0) {
+            // this needs to get the text before and after the current offset
+        } else {
+            // Set the cursor back to the beginning of the current line if 
+            // it is not at the bottom of the textArea
+            if (cursor.y < this.editorInstance.screen.height - 1) {
+                this.editorInstance.program.cursorPos(cursor.y, 1);
+            }
+            else {
+                // Scroll the textArea by one
+                this.editorInstance.textArea.textArea.scroll(1);
+                // Put the cursor at the start of the current line
+                this.editorInstance.program.cursorPos(cursor.y - 1, 1);
+                // Increase the verticalScrollOffset by one to match the blessed scroll index
+                this.editorInstance.textArea.verticalScrollOffset++;
+            }
+            // Render the cursor change
+            this.editorInstance.screen.render();
+            // update the line to in the 'real' text
+            this.editorInstance.textArea.shadowContent.splice(currentLineOffset, 0, '');
+        }
     }
 
     tabHandler() {
