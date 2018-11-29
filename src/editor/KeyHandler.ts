3a6e97f5bac1c5bbe657da4516a19683042ce48c
Lilith Tundrus <echofox123@gmail.com>
2018-11-29 09:38:43 -0500

    right arrow now properly flows down to the next line

--- src/editor/KeyHandler.ts
+++ src/editor/KeyHandler.ts
@@ -10,9 +10,8 @@ import * as fs from 'fs';
 // This file contains the class for handling key events for the Editor Class's
 // textArea UI component
 
-// TODO: Better handle the vertical offset things
 // TODO: any offsets for horizontal need to be handled internally in the textarea class
-
+// TODO: Refactor and split this all out...
 export default class KeyHandler {
 
     // The editorInstance allows us to access features from the Editor class instance to do things
@@ -579,8 +578,6 @@ export default class KeyHandler {
         // This callback returns an err and data object, the data object has the x/y 
         // position of the cursor
         this.editorInstance.program.getCursor((err, cursor) => {
-            // Ignore errors until a proper error system is put in place
-            if (err) return;
             // If the cursor is not at the end of the line the cursor is on, move it backwards one
             if (cursor.x > 2) {
                 this.editorInstance.program.cursorBackward();
@@ -610,9 +607,6 @@ export default class KeyHandler {
         // This callback returns an err and data object, the data object has the x/y 
         // position of the cursor
         this.editorInstance.program.getCursor((err, cursor) => {
-            // Ignore errors until a proper error system is put in place
-            if (err) return;
-
             // Variable to get the current offset number for the line the cursor is on,
             // including the scrolling position of the textArea
             let currentLineOffset = this.editorInstance.textArea.calculateScrollingOffset(cursor);
@@ -666,7 +660,7 @@ export default class KeyHandler {
                 // Horiztonally scroll the text right by 1 if the current line is greater than the
                 // width of the editing window
             } else {
-                // TODO: prevent the cursor from moving past the current line's text length
+                // If the current line's length is greater than the viewoffset plus the cursor position
                 if (shadowLineText.length >= this.editorInstance.textArea.viewOffSet + cursor.x - 1) {
                     // Increase the horizontal view offset of the textArea by one
                     this.editorInstance.textArea.viewOffSet++;
@@ -680,7 +674,50 @@ export default class KeyHandler {
                     this.editorInstance.program.cursorPos(cursor.y - 1, screenWidthWithUIOffsets);
                 } else {
                     // Reflow down to the next line
+                    if (cursor.y < this.editorInstance.screen.height - 1) {
 
+                        this.editorInstance.screen.render();
+
+                        this.editorInstance.textArea.leftShiftText(this.editorInstance.textArea.viewOffSet);
+                        this.editorInstance.textArea.viewOffSet = 0;
+                        // Since the cursor is on the next line, the offset needs to be updated to reflect that
+                        this.editorInstance.textArea.verticalScrollOffset++;
+                        // Render the text reform
+                        this.editorInstance.screen.render();
+
+                        // reflow down to the next line and update the vertical offset
+                        this.editorInstance.program.cursorPos(cursor.y, 1);
+                    }
+                    // Scroll the text down by one if the cursor is at the bottom of the textArea
+                    else if (cursor.y == this.editorInstance.screen.height - 1) {
+                        // This AND check prevents a crash that occurs when on the last line of the 
+                        // text being edited
+                        if (this.editorInstance.textArea.textArea.getScrollPerc() !== 100) {
+
+                            // Scroll the textArea down by one
+                            this.editorInstance.textArea.textArea.scroll(1);
+                            // Ensure the scroll rendered before the text reform function is called
+                            this.editorInstance.screen.render();
+
+                            // Make sure that the next line is on the right horizontal scroll index
+                            this.editorInstance.textArea.reformTextDownArrow();
+                            // Render the text reform
+                            this.editorInstance.screen.render();
+
+                            // Keep the cursor in its previous position
+                            // For some reason setting the y on this to 2 scrolls more 'smoothly' than 3
+                            // (less cursor jank)
+                            let relativeBottomHeight = this.editorInstance.screen.height - 2;
+                            this.editorInstance.program.cursorPos(relativeBottomHeight, 1);
+                            // Render the cursor change
+                            this.editorInstance.screen.render();
+                            // Increase the verticalScrollOffset by one to match the blessed scroll index
+                            this.editorInstance.textArea.verticalScrollOffset++;
+                            this.editorInstance.textArea.internalVerticalOffset++;
+                        }
+                        // Also make sure that if the cursor is at the bottom of the screen that ALL
+                        // of the text scrolls
+                    }
                     // Also make sure that if the cursor is at the bottom of the screen that ALL
                     // of the text scrolls
                 }
@@ -694,8 +731,6 @@ export default class KeyHandler {
         // This callback returns an err and data object, the data object has the x/y 
         // position of the cursor
         this.editorInstance.program.getCursor((err, cursor) => {
-            // Ignore errors until a proper error system is put in place
-            if (err) return;
 
             // If the cursor is in top bound of the editing window plus the menubar height
             if (cursor.y > 3) {
@@ -739,8 +774,6 @@ export default class KeyHandler {
         // This callback returns an err and data object, the data object has the x/y position 
         // of the cursor
         this.editorInstance.program.getCursor((err, cursor) => {
-            // Ignore errors until a proper error system is put in place
-            if (err) return;
 
             // This visually keeps the cursor in bottom bound of the editing window,
             // accounting for the extra the statusbar height
